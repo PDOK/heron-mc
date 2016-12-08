aef4fe847a155d6d32285d1a8c379f263e5af2d8
Frank Steggink <frank@steggink.it>
2016-12-08 11:21:44 +0100

    Temporary removed pip-install file due to name conflict

--- Docker/init/pip-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-pip install --upgrade pip
-pip install --ignore-installed --require-hashes --no-cache-dir -r ${DIR}/requirements.txt
